17e22e4a0349089b6a2d0e4e82c59f3f56844bd5
akshaydibe <akshaydibe@gmail.com>
2026-05-24 16:01:36 +0530

    Initial commit

---
+++ ProductCatalog/ProductCatalog/ProductCatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCatalogApp.swift
+//  ProductCatalog
+//
+//  Created by Akshay Dibe on 24/05/26.
+//
+
+import SwiftUI
+
+@main
+struct ProductCatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
